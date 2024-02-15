ae4c4d3c8408fc8a2660a67c87a5b338c1ff0e1b
Edgar Chairez <58444183+EdgarChairez@users.noreply.github.com>
2024-02-14 18:09:16 -0700

    Add files via upload
    Se agregan funciones, imprime todos los registros de inicio.

--- Obtener Directorio.py
+++ Obtener Directorio.py
@@ -18,18 +18,17 @@ datos_log=open("datos_log_"+str((date.today())) + ".txt","a")
 # Crea LOG #
 
 
-#driver = webdriver.Firefox()                               # Mostrar proceso
+driver = webdriver.Firefox()                               # Mostrar proceso
 # Ocultar navegador
-log.write('-- Inicia servicio - ' + now + ' --\n')
+"""log.write('-- Inicia servicio - ' + now + ' --\n')
 datos_log.write('-- Inicia servicio - ' + now + ' --')
 firefox_options = Options()                                 # Ocultar proceso
 firefox_options.add_argument('--headless')                  # Ocular proceso
-driver = webdriver.Firefox(options=firefox_options)         # Ocularr proceso
+driver = webdriver.Firefox(options=firefox_options)         # Ocularr proceso"""
 print('. ')
 # Ocultar navegador
 
 
-#driver.get('https://www.coppel.com/')
 driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
 
@@ -49,7 +48,7 @@ except Exception as e:
 
 # Seleccionar Directory #
 try:
-    log.write('\n-- Selecciona Directory  - ' + now + ' --\n')
+    log.write('\n-- Selecciona Directory --\n')
     WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][contains(.,'Directory')]"))).click()
     print('. ')
     datos_log.write('\n.')
@@ -62,7 +61,7 @@ except Exception as e:
 def filtro(locacion):
     print(".\nSe realiza busqueda de: " + locacion)
     datos_log.write(".\nSe realiza busqueda de: " + locacion + " - "+ now + "\n")
-    log.write('\n-- Selecciona Filtro  - ' + now + ' --\n')
+    log.write('\n-- Selecciona Filtro --\n')
     try:
         # Seleccionar selector
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[3]/div[1]/div[2]"))).click()
@@ -94,6 +93,17 @@ def cantidad():
     return cantidad
     print('. ')
 # Obtener cantidad #
+
+
+# Obtener total de registro #
+def total():
+    total = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]"))).text
+    numero = ''
+    for c in total:
+        if c.isnumeric():
+            numero = numero + c
+    return int(numero)
+# Obtener total de registro #
 
 
 # Obtener Datos #
@@ -138,7 +148,7 @@ def datos():
             "Email": email
         }
         return personas
-        log.write('\n-- Se obienen datos  - ' + now + ' --\n')
+        log.write('\n-- Se obienen datos --\n')
     except WDE as e:
         print("Error al obtener datos: ", e)
         log.write("Error al obtener datos: ", e)
@@ -146,63 +156,63 @@ def datos():
 # Obtener Datos #
 
 
-# Ejecutar servicio con filtro #
-def ejecutar(bus):              
-    filtro(bus)                 # Aplicar filtro #
-    cantidad()                  # Obtener cantidad de contactos
+# Scroll para obtener el total de registros #
+def scrol():
+    scr1 = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[2]/div[1]/div[2]")))
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", scr1)
+# Scroll para obtener el total de registros #
+
+
+# Imprimir diccionario
+def persona():
+    personas = datos()
+    for clave, valor in personas.items():
+        print("{0}: {1}".format(clave, valor))
+        datos_log.write("{0}: {1}".format(clave, valor) + "\n")
+    print("-")
+    datos_log.write("-\n")
+# Imprimir diccionario
+
+def ejecutarFunc():
     if cantidad() > 0:
-        print("Se encontraron " + str(cantidad()) + " registros")
-        datos_log.write("Se encontraron " + str(cantidad()) + " registros\n")
+        print("Se encontraron " + str(cantidad()) + " registros\n-")
+        datos_log.write("Se encontraron " + str(cantidad()) + " registros\n-\n")
         for x in range(cantidad()):
             i=x+1
             seleccionar(i)
-            #time.sleep(2)
-            print(datos())
-            datos_log.write(str(datos()) + "\n")
-            #time.sleep(2)
+            persona()
             seleccionar(i)
     else:
         print("No se encontraron datos")
         datos_log.write("No se encontraron datos\n.")
         print(".")
-# Ejecutar servicio con filtro #
-
-
-# Ejecutar servicio sin filtro #
-def ejecutar():
-    cantidad()                  # Obtener cantidad de contactos
-    if cantidad() > 0:
-        print("Se encontraron " + str(cantidad()) + " registros")
-        datos_log.write("Se encontraron " + str(cantidad()) + " registros\n")
-        for x in range(cantidad()):
-            i=x+1
-            seleccionar(i)
-            #time.sleep(2)
-            print(datos())
-            datos_log.write(str(datos()) + "\n")
-            #time.sleep(2)
-            seleccionar(i)
-    else:
-        print("No se encontraron datos")
-        datos_log.write("No se encontraron datos\n.")
-        print(".")
-# Ejecutar servicio sin filtro #
 
 opcion = input("¿Ejecutar servicio con filtros? ")
 opcion = opcion.upper()
 
+
 if opcion == "SI":
-    ejecutar('New York Sales Office')          # Ejecutar con filtro
-    ejecutar('HQ - CA, USA')                   # Ejecutar con filtro
-    ejecutar('New York Sales Office')          # Ejecutar con filtro
-    ejecutar('Texas R&D')                      # Ejecutar con filtro
+    # Ejecutar servicio con filtro #
+    def ejecutar(bus):              
+        filtro(bus)                 # Aplicar filtro #
+        ejecutarFunc()              # Obtiene datos
+    # Ejecutar servicio con filtro #
+    ejecutar('Canadian Regional HQ')
+    ejecutar('HQ - CA, USA')
+    ejecutar('New York Sales Office')
+    ejecutar('Texas R&D')
 else:
-    ejecutar()                                  # Ejecutar sin filtro
+    # Ejecutar servicio sin filtro #
+    def ejecutar():
+        while total() > cantidad():
+            scrol()
+        ejecutarFunc()              # Obtiene datos
+    ejecutar()                      # Ejecutar sin filtro
 
 
 # Cerrar
 driver.quit()
-log.write('\n-- Finaliza Servicio  - ' + now + ' --\n\n')
-datos_log.write('..\n-- Finaliza Servicio  - ' + now + ' -- \n.\n')
+log.write('\n-- Finaliza servicio --\n\n')
+datos_log.write('..\n-- Finaliza servicio  - ' + now + ' -- \n.\n')
 print('.\nFinaliza el proceso')
 log.close()
